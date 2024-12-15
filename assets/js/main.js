d7a7c84eaf15b551afbf2b5d27a981bf416abccf
Uraani@live.fi <105335740+Ultikynnys@users.noreply.github.com>
2024-12-15 04:41:55 +0200

    no message

---
+++ assets/js/main.js
@@ -0,0 +1,428 @@
+/*
+	Forty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+
+document.addEventListener("click", function (event) {
+	if (event.target.matches("[data-switcher] button")) {
+		const container = event.target.closest("[data-switcher]");
+		const images = container.querySelectorAll("img");
+
+		// Toggle the active class on each image
+		images.forEach(img => img.classList.toggle("active"));
+	}
+});
+
+
+(function ($) {
+
+	const template2 = document.createElement('template2');
+
+	template2.innerHTML = `
+
+
+    <style>
+        .container {
+            text-align: center;
+            margin: 5px;
+        }
+        .title {
+            margin-bottom: 10px;
+        }
+        .links {
+            display: flex;
+            justify-content: center;
+			margin-top: 10px;
+            gap: 25px;
+        }
+        .links a {
+        }
+    </style>
+
+		<header id="header" class="alt">
+		
+		
+	<div class="container">
+    <a id="kek" href="index.html" class="logo"><strong>Ultikynnys | Ubeid Hussein</strong> <span>Portfolio</span></a>
+    <div class="links">
+        <p class="title"><a href="3d.html" class="link">3D Stuff</a></p>
+        <p class="title"><a href="tools.html" class="link">Tools | Products</a></p>
+        <p class="title"><a href="games.html" class="link">Games</a></p>
+        <p class="title"><a href="videos.html" class="link">Videos</a></p>
+    </div>
+	</div>
+
+		</header>
+
+		<section id="contact">
+			<div class="inner">
+				<section class="split">
+					<section>
+						<div class="contact-method">
+							<span class="icon solid alt fa-envelope"></span>
+							<h3>Email</h3>
+							<a href="mailto:Hussein.Ubeid@outlook.com">Hussein.Ubeid@outlook.com</a>
+						</div>
+					</section>
+					<section>
+						<div class="contact-method">
+							<span class="icon solid alt fa-home"></span>
+							<h3>Location</h3>
+							<span>Somewhere in Finland :D</span>
+						</div>
+					</section>
+				</section>
+			</div>
+		</section>
+		<footer id="footer">
+		<div class="inner">
+			<ul class="icons">
+				<li><a href="https://twitter.com/ultikynnys" class="icon brands fa-twitter"><span class="label"></span></a></li>
+				<li><a href="https://www.instagram.com/ultikynnysreal/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+				<li><a href="https://github.com/R60D" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+				<li><a href="https://www.linkedin.com/in/ubeid-hussein-31ba59204/" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+				<li><a href="CV.html" class="icon solid fa-envelope-open-text"><span class="label">LinkedIn</span></a></li>
+			</ul>
+			<ul class="copyright">
+				<li>&copy; Ubeid Hussein</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
+			</ul>
+		</div>
+		</footer>
+	</div>
+
+	
+	`;
+
+	document.body.appendChild(template2)
+
+
+
+	var $window = $(window),
+		$body = $('body'),
+		$wrapper = $('#wrapper'),
+		$header = $('#header'),
+		$banner = $('#banner');
+
+	// Breakpoints.
+	breakpoints({
+		xlarge: ['1281px', '1680px'],
+		large: ['981px', '1280px'],
+		medium: ['737px', '980px'],
+		small: ['481px', '736px'],
+		xsmall: ['361px', '480px'],
+		xxsmall: [null, '360px']
+	});
+
+	/**
+	 * Applies parallax scrolling to an element's background image.
+	 * @return {jQuery} jQuery object.
+	 */
+	$.fn._parallax = (browser.name == 'ie' || browser.name == 'edge' || browser.mobile) ? function () { return $(this) } : function (intensity) {
+
+		var $window = $(window),
+			$this = $(this);
+
+		if (this.length == 0 || intensity === 0)
+			return $this;
+
+		if (this.length > 1) {
+
+			for (var i = 0; i < this.length; i++)
+				$(this[i])._parallax(intensity);
+
+			return $this;
+
+		}
+
+		if (!intensity)
+			intensity = 0.25;
+
+		$this.each(function () {
+
+			var $t = $(this),
+				on, off;
+
+			on = function () {
+
+				$t.css('background-position', 'center 100%, center 100%, center 0px');
+
+				$window
+					.on('scroll._parallax', function () {
+
+						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
+
+						$t.css('background-position', 'center ' + (pos * (-1 * intensity)) + 'px');
+
+					});
+
+			};
+
+			off = function () {
+
+				$t
+					.css('background-position', '');
+
+				$window
+					.off('scroll._parallax');
+
+			};
+
+			breakpoints.on('<=medium', off);
+			breakpoints.on('>medium', on);
+
+		});
+
+		$window
+			.off('load._parallax resize._parallax')
+			.on('load._parallax resize._parallax', function () {
+				$window.trigger('scroll');
+			});
+
+		return $(this);
+
+	};
+
+	// Play initial animations on page load.
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
+
+	// Clear transitioning state on unload/hide.
+	$window.on('unload pagehide', function () {
+		window.setTimeout(function () {
+			$('.is-transitioning').removeClass('is-transitioning');
+		}, 250);
+	});
+
+	// Fix: Enable IE-only tweaks.
+	if (browser.name == 'ie' || browser.name == 'edge')
+		$body.addClass('is-ie');
+
+	// Scrolly.
+	$('.scrolly').scrolly({
+		offset: function () {
+			return $header.height() - 2;
+		}
+	});
+
+	// Tiles.
+	var $tiles = $('.tiles > article');
+
+	$tiles.each(function () {
+
+		var $this = $(this),
+			$image = $this.find('.image'), $img = $image.find('img'),
+			$link = $this.find('.link'),
+			x;
+
+		// Image.
+
+		// Set image.
+		$this.css('background-image', 'url(' + $img.attr('src') + ')');
+
+		// Set position.
+		if (x = $img.data('position'))
+			$image.css('background-position', x);
+
+		// Hide original.
+		$image.hide();
+
+		// Link.
+		if ($link.length > 0) {
+
+			$x = $link.clone()
+				.text('')
+				.addClass('primary')
+				.appendTo($this);
+
+			$link = $link.add($x);
+
+			$link.on('click', function (event) {
+
+				var href = $link.attr('href');
+
+				// Prevent default.
+				event.stopPropagation();
+				event.preventDefault();
+
+				// Target blank?
+				if ($link.attr('target') == '_blank') {
+
+					// Open in new tab.
+					window.open(href);
+
+				}
+
+				// Otherwise ...
+				else {
+
+					// Start transitioning.
+					$this.addClass('is-transitioning');
+					$wrapper.addClass('is-transitioning');
+
+					// Redirect.
+					window.setTimeout(function () {
+						location.href = href;
+					}, 500);
+
+				}
+
+			});
+
+		}
+
+	});
+
+	// Header.
+	if ($banner.length > 0
+		&& $header.hasClass('alt')) {
+
+		$window.on('resize', function () {
+			$window.trigger('scroll');
+		});
+
+		$window.on('load', function () {
+
+			$banner.scrollex({
+				bottom: $header.height() + 10,
+				terminate: function () { $header.removeClass('alt'); },
+				enter: function () { $header.addClass('alt'); },
+				leave: function () { $header.removeClass('alt'); $header.addClass('reveal'); }
+			});
+
+			window.setTimeout(function () {
+				$window.triggerHandler('scroll');
+			}, 100);
+
+		});
+
+	}
+
+	// Banner.
+	$banner.each(function () {
+
+		var $this = $(this),
+			$image = $this.find('.image'), $img = $image.find('img');
+
+		// Parallax.
+		$this._parallax(0.275);
+
+		// Image.
+		if ($image.length > 0) {
+
+			// Set image.
+			$this.css('background-image', 'url(' + $img.attr('src') + ')');
+
+			// Hide original.
+			$image.hide();
+
+		}
+
+	});
+
+	// Menu.
+	var $menu = $('#menu'),
+		$menuInner;
+
+	$menu.wrapInner('<div class="inner"></div>');
+	$menuInner = $menu.children('.inner');
+	$menu._locked = false;
+
+	$menu._lock = function () {
+
+		if ($menu._locked)
+			return false;
+
+		$menu._locked = true;
+
+		window.setTimeout(function () {
+			$menu._locked = false;
+		}, 350);
+
+		return true;
+
+	};
+
+	$menu._show = function () {
+
+		if ($menu._lock())
+			$body.addClass('is-menu-visible');
+
+	};
+
+	$menu._hide = function () {
+
+		if ($menu._lock())
+			$body.removeClass('is-menu-visible');
+
+	};
+
+	$menu._toggle = function () {
+
+		if ($menu._lock())
+			$body.toggleClass('is-menu-visible');
+
+	};
+
+	$menuInner
+		.on('click', function (event) {
+			event.stopPropagation();
+		})
+		.on('click', 'a', function (event) {
+
+			var href = $(this).attr('href');
+
+			event.preventDefault();
+			event.stopPropagation();
+
+			// Hide.
+			$menu._hide();
+
+			// Redirect.
+			window.setTimeout(function () {
+				window.location.href = href;
+			}, 250);
+
+		});
+
+	$menu
+		.appendTo($body)
+		.on('click', function (event) {
+
+			event.stopPropagation();
+			event.preventDefault();
+
+			$body.removeClass('is-menu-visible');
+
+		})
+		.append('<a class="close" href="#menu">Close</a>');
+
+	$body
+		.on('click', 'a[href="#menu"]', function (event) {
+
+			event.stopPropagation();
+			event.preventDefault();
+
+			// Toggle.
+			$menu._toggle();
+
+		})
+		.on('click', function (event) {
+
+			// Hide.
+			$menu._hide();
+
+		})
+		.on('keydown', function (event) {
+
+			// Hide on escape.
+			if (event.keyCode == 27)
+				$menu._hide();
+
+		});
+
+})(jQuery);
